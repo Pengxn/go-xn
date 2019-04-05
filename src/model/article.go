6565f9b9173bf95fa1b8b5cefbe2386b19202984
huiyifyj <jxfengyijie@gmail.com>
2019-04-06 01:49:51 +0800

    Remove common model, and update Article model to support xorm

--- src/model/article.go
+++ src/model/article.go
@@ -4,12 +4,12 @@ import "time"
 
 // Article model
 type Article struct {
-	ID         uint64
-	Title      string
-	Content    string
-	Views      uint64
-	Status     int
-	CreateTime time.Time
-	UpdateTime time.Time
-	DeleteTime time.Time
+	ID         uint64    `form:"article_id" xorm:"bigint(20) notnull autoincr"`
+	Title      string    `form:"title" xorm:"text notnull"`
+	Content    string    `form:"content" xorm:"longtext notnull"`
+	Views      uint64    `form:"views" xorm:"bigint(20) notnull default(0)"`
+	Status     int       `form:"status" xorm:"tinyint(1) notnull default(0)"`
+	CreateTime time.Time `form:"create_time" xorm:"datetime created notnull"`
+	UpdateTime time.Time `form:"update_time" xorm:"datetime updated notnull"`
+	DeleteTime time.Time `form:"delete_time" xorm:"datetime deleted notnull"`
 }
