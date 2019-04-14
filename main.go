baecf94070d8fdb7726fd7e6115d2f1551c8c0da
huiyifyj <jxfengyijie@gmail.com>
2019-04-14 17:17:40 +0800

    Move part of coding to app package

--- main.go
+++ main.go
@@ -1,31 +1,9 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/gin-gonic/gin"
-
-	"go-xn/src"
-)
-
-// Information for Go-xn
-const (
-	Version = "1.0.0"
-	URL     = "https://xn--02f.com"
-	banner  = `
-    ____
-   / ___| ___        __  ___ __
-  | |  _ / _ \  ____ \ \/ / '_ \
-  | |_| | (_) ||____| >  <| | | |
-   \____|\___/       /_/\_\_| |_|
-
-`
+	app "go-xn/src"
 )
 
 func main() {
-	fmt.Printf(banner)
-
-	gin.SetMode(gin.ReleaseMode)
-
 	app.Run()
 }
--- src/server.go
+++ src/server.go
@@ -1,14 +1,34 @@
 package app
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 
 	"go-xn/src/route"
 )
 
+// Information for Go-xn
+const (
+	Version = "1.0.0"
+	URL     = "https://xn--02f.com"
+	banner  = `
+    ____
+   / ___| ___        __  ___ __
+  | |  _ / _ \  ____ \ \/ / '_ \
+  | |_| | (_) ||____| >  <| | | |
+   \____|\___/       /_/\_\_| |_|
+
+`
+)
+
 // Run is the entry point to the server app.
-// Parses the arguments slice and routes.
+// Parses the arguments routes and others.
 func Run() {
+	gin.SetMode(gin.ReleaseMode)
+
+	fmt.Printf(banner)
+
 	g := gin.Default()
 
 	route.InitRoutes(g)
