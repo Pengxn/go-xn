b27baddafd9f9ffd10cc33cb7974b39534a542a7
huiyifyj <jxfengyijie@gmail.com>
2019-04-20 01:04:09 +0800

    Add no route and no method handler

--- src/route/home.go
+++ src/route/home.go
@@ -8,14 +8,24 @@ import (
 
 // InitRoutes will init all routes.
 func InitRoutes(g *gin.Engine) {
-	g.LoadHTMLGlob("web/template/*")
-
 	g.GET("/", controller.HomePage)
 
 	// Page number handler
 	Page := g.Group("/page")
-	Page.Any("/:pageNum", controller.ByPageNum)
+	Page.GET("/:pageNum", controller.ByPageNum)
 
 	staticRoutes(g)
 	articlesRoutes(g)
+
+	g.LoadHTMLFiles("web/404.html")
+
+	// No route
+	g.NoRoute(func(c *gin.Context) {
+		c.HTML(404, "404.html", gin.H{})
+	})
+
+	// No Method
+	g.NoMethod(func(c *gin.Context) {
+		c.HTML(404, "404.html", gin.H{})
+	})
 }
