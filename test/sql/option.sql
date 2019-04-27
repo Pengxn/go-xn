7a156ea9667a9f3f8c5a42756fb660bcfef62cb3
huiyifyj <jxfengyijie@gmail.com>
2019-04-27 15:57:51 +0800

    Rename data.sql to article.sql and create option.sql
    - Rename data.sql to article.sql and move it to sql folder.
    - Create option.sql for 'Option' model.

---
+++ test/sql/option.sql
@@ -0,0 +1,23 @@
+/**
+ * This database file is used to test
+ *
+ * (c) MIT 2019
+ */
+
+-- --------------------------------
+-- Table structure for option
+-- --------------------------------
+DROP TABLE IF EXISTS `option`;
+CREATE TABLE `option` (
+    `option_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    `option_name` varchar(255) NOT NULL COMMENT 'The name of option',
+    `option_value` longtext NOT NULL COMMENT 'The value of option',
+    PRIMARY KEY (`option_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8mb4;
+-- --------------------------------
+-- Records of article
+-- --------------------------------
+INSERT INTO `option` VALUES (1, "site_url", "https://xn--02f.com");
+INSERT INTO `option` VALUES (2, "blog_name", "xn-02f Lab");
+INSERT INTO `option` VALUES (3, "blog_description", "✍ The platform for publishing and running your blog.");
+INSERT INTO `option` VALUES (4, "users_can_register", "0");
