75c314aed7018a7caf748385dc4a472308fdd62e
huiyifyj <jxfengyijie@gmail.com>
2019-05-16 17:19:33 +0800

    Add homedir file to detect home directory for executing user

---
+++ src/config/homedir.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"errors"
+	"os"
+	"runtime"
+)
+
+// HomeDir detectes and returns the home directory for the executing user.
+// This uses an OS-specific method for discovering the home directory.
+// An error is returned if a home directory cannot be detected.
+func HomeDir() (string, error) {
+	var (
+		home string
+		err  error
+	)
+
+	if runtime.GOOS == "windows" {
+		home, err = dirWindows()
+	} else {
+		// Unix-like system, so just assume Unix
+		home, err = dirUnix()
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	return home, err
+}
+
+func dirWindows() (string, error) {
+	home := os.Getenv("USERPROFILE")
+
+	if home == "" {
+		return "", errors.New("Can't find 'USERPROFILE' environment variable")
+	}
+
+	return home, nil
+}
+
+func dirUnix() (string, error) {
+	home := os.Getenv("HOME")
+
+	if home == "" {
+		return "", errors.New("Can't find 'USERPROFILE' environment variable")
+	}
+
+	return home, nil
+}
