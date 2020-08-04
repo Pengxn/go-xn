df9c65aea5aacc01c298bf72d1257b348081ab54
huiyifyj <jxfengyijie@gmail.com>
2020-08-04 18:11:57 +0800

    Move files and fix typo
    - Move test files and refactor them.
    - Fix typo: configuratioin => configuration.

--- src/config/config_test.go
+++ src/config/config_test.go
@@ -4,14 +4,12 @@ import (
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
-
-	"go-xn/src/config"
 )
 
 func TestDBUrl(t *testing.T) {
 	Convey("Test DB url", t, func() {
 		want := "root:password@tcp(127.0.0.1:3306)/fyj?charset=utf8"
-		url := config.DBUrl()
+		url := DBUrl()
 
 		So(url, ShouldEqual, want)
 	})
--- src/config/homedir_test.go
+++ src/config/homedir_test.go
@@ -5,14 +5,12 @@ import (
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
-
-	"go-xn/src/config"
 )
 
 func TestHomeDir(t *testing.T) {
 	Convey("Test Home Directory Path", t, func() {
 		home, _ := user.Current()
 
-		So(config.HomeDir(), ShouldEqual, home.HomeDir)
+		So(HomeDir(), ShouldEqual, home.HomeDir)
 	})
 }
