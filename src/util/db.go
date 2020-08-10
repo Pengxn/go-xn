acdcccd48a85a2040fa41179ff6e7c811a021c07
huiyifyj <jxfengyijie@gmail.com>
2020-08-10 13:33:59 +0800

    Remove ping test for database

--- src/util/db.go
+++ src/util/db.go
@@ -13,13 +13,8 @@ import (
 // and return *xorm.Engine
 func DBEngine() *xorm.Engine {
 	orm, err := xorm.NewEngine("mysql", config.DBUrl())
-
 	if err != nil {
 		log.Fatalln("Can't connect your DB.", err.Error())
-	}
-
-	if err = orm.Ping(); err != nil {
-		log.Fatalln("Your DB can't work it normally.", err.Error())
 	}
 
 	return orm
