2b0450100b053e4aa9accc3e5b8c3383a3943e87
huiyifyj <jxfengyijie@gmail.com>
2020-08-15 02:22:20 +0800

    Upload application manifest XML and shell files
    - Upload application manfest file `manifest.xml` for windows system.
      And it describes and identifies the shared and private side-by-side assemblies that an application should bind to at run time.
      More information to https://docs.microsoft.com/en-us/windows/win32/sbscs/application-manifests
    - Upload `rcedit.sh` file that is used to edit resources of exe.
      You can run this script file after installing [rcedit](https://github.com/electron/rcedit).
      More information to https://github.com/electron/rcedit

---
+++ script/windows/rcedit.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+VERSION="0.0.0-beta"
+
+rcedit ./build/go-xn.exe \
+        --set-icon "./build/web/icons/xn-02f.ico" \
+        --set-version-string CompanyName "xn-02f Lab" \
+        --set-version-string ProductName "xn-02f Lab" \
+        --set-version-string FileDescription "✍ The platform for publishing and running your blog." \
+        --set-version-string OriginalFilename "go-xn.exe" \
+        --set-version-string InternalName "go-xn" \
+        --set-version-string LegalCopyright "(c) All Go-xn contributors. Zlib LICENSE." \
+        --set-file-version ${VERSION} \
+        --set-product-version ${VERSION} \
+        --application-manifest ./script/windows/manifest.xml
