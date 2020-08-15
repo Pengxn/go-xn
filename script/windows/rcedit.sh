89febe3995bb61567ff3ec2e02e879b82296b584
huiyifyj <jxfengyijie@gmail.com>
2020-08-15 21:13:04 +0800

    Update icon file path
    - Update icon file path when use rcedit to edit executable file for Windows.
    - Add annotate information.

--- script/windows/rcedit.sh
+++ script/windows/rcedit.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+# Make sure the `rcedit` is installed
+# https://github.com/electron/rcedit
+
 VERSION="0.0.0-beta"
 
 rcedit ./build/go-xn.exe \
-        --set-icon "./build/web/icons/xn-02f.ico" \
+        --set-icon "./web/icons/xn-02f.ico" \
         --set-version-string CompanyName "xn-02f Lab" \
         --set-version-string ProductName "xn-02f Lab" \
         --set-version-string FileDescription "✍ The platform for publishing and running your blog." \
         --set-version-string OriginalFilename "go-xn.exe" \
         --set-version-string InternalName "go-xn" \
-        --set-version-string LegalCopyright "(c) All Go-xn contributors. Zlib LICENSE." \
+        --set-version-string LegalCopyright "(c) All Go-xn Contributors. Zlib LICENSE." \
         --set-file-version ${VERSION} \
         --set-product-version ${VERSION} \
         --application-manifest ./script/windows/manifest.xml
