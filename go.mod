d5848425be73b290e091084588afa1cf9c84b0f8
huiyifyj <jxfengyijie@gmail.com>
2020-08-27 23:28:00 +0800

    Upgrade ini and add log common package
    - Upgrades [ini](https://github.com/go-ini/ini) from 1.60.1 to 1.60.2.
      Changelog: https://github.com/go-ini/ini/compare/v1.60.1...v1.60.2
    - Add log common function, use [logrus](https://github.com/sirupsen/logrus) package.

--- go.mod
+++ go.mod
@@ -2,10 +2,11 @@ module github.com/Pengxn/go-xn
 
 require (
 	github.com/gin-gonic/gin v1.6.3
-	github.com/go-ini/ini v1.60.1
+	github.com/go-ini/ini v1.60.2
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/lib/pq v1.8.0
 	github.com/mattn/go-sqlite3 v1.14.1
+	github.com/sirupsen/logrus v1.6.0
 	github.com/smartystreets/goconvey v1.6.4
 	github.com/urfave/cli/v2 v2.2.0
 	xorm.io/xorm v1.0.3
---
+++ src/util/log.go
@@ -0,0 +1,51 @@
+package util
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+var logger *logrus.Logger
+
+func init() {
+	logger = logrus.New()
+}
+
+func Error(args ...interface{}) {
+	logger.Error(args...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	logger.Errorf(format, args...)
+}
+
+func Info(args ...interface{}) {
+	logger.Info(args...)
+}
+
+func Infof(format string, args ...interface{}) {
+	logger.Infof(format, args...)
+}
+
+func Fatal(args ...interface{}) {
+	logger.Fatal(args...)
+}
+
+func Fatalf(format string, args ...interface{}) {
+	logger.Fatalf(format, args...)
+}
+
+func Warn(args ...interface{}) {
+	logger.Warn(args...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	logger.Warnf(format, args...)
+}
+
+func Panic(args ...interface{}) {
+	logger.Panic(args...)
+}
+
+func Panicf(format string, args ...interface{}) {
+	logger.Panicf(format, args...)
+}
