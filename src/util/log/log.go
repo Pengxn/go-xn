25a03c79b1e79f31858b2df35ecad8e9e4593e29
huiyifyj <jxfengyijie@gmail.com>
2020-08-30 23:21:16 +0800

    Move log.go to src/util/log subfolder
    - Move 'log.go' file to src/util/log subfolder.
    - Rename package: util => log.
    - Add `~ln` functions: Errorln, Infoln, Fatalln, Warnln and Panicln.

--- src/util/log/log.go
+++ src/util/log/log.go
@@ -1,4 +1,4 @@
-package util
+package log
 
 import (
 	"github.com/sirupsen/logrus"
@@ -18,12 +18,20 @@ func Errorf(format string, args ...interface{}) {
 	logger.Errorf(format, args...)
 }
 
+func Errorln(format string, args ...interface{}) {
+	logger.Errorln(args...)
+}
+
 func Info(args ...interface{}) {
 	logger.Info(args...)
 }
 
 func Infof(format string, args ...interface{}) {
 	logger.Infof(format, args...)
+}
+
+func Infoln(args ...interface{}) {
+	logger.Infoln(args...)
 }
 
 func Fatal(args ...interface{}) {
@@ -34,6 +42,10 @@ func Fatalf(format string, args ...interface{}) {
 	logger.Fatalf(format, args...)
 }
 
+func Fatalln(args ...interface{}) {
+	logger.Fatalln(args...)
+}
+
 func Warn(args ...interface{}) {
 	logger.Warn(args...)
 }
@@ -42,10 +54,18 @@ func Warnf(format string, args ...interface{}) {
 	logger.Warnf(format, args...)
 }
 
+func Warnln(args ...interface{}) {
+	logger.Warnln(args...)
+}
+
 func Panic(args ...interface{}) {
 	logger.Panic(args...)
 }
 
 func Panicf(format string, args ...interface{}) {
 	logger.Panicf(format, args...)
+}
+
+func Panicln(format string, args ...interface{}) {
+	logger.Panicln(args...)
 }
