c8a450e16591ade9b7707943e6ac7766d9c835c1
huiyifyj <jxfengyijie@gmail.com>
2020-09-01 01:19:14 +0800

    Increase test coverage and add gomonkey library
    - Increase tests for dirWindows and dirUnix functions.
    - [gomonkey](github.com/agiledragon/gomonkey) is a library to make monkey patching in unit tests easy.

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/Pengxn/go-xn
 
 require (
+	github.com/agiledragon/gomonkey v2.0.2+incompatible
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-ini/ini v1.60.2
 	github.com/go-sql-driver/mysql v1.5.0
--- src/config/homedir_test.go
+++ src/config/homedir_test.go
@@ -1,9 +1,12 @@
 package config
 
 import (
+	"errors"
+	"os"
 	"os/user"
 	"testing"
 
+	. "github.com/agiledragon/gomonkey"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -12,5 +15,53 @@ func TestHomeDir(t *testing.T) {
 		home, _ := user.Current()
 
 		So(HomeDir(), ShouldEqual, home.HomeDir)
+	})
+}
+
+func TestDirWindows(t *testing.T) {
+	Convey("Test dirWindows and error is nil.", t, func() {
+		p := ApplyFunc(os.Getenv, func(_ string) string {
+			return "C:\\Users"
+		})
+		defer p.Reset()
+
+		home, err := dirWindows()
+		So(home, ShouldEqual, "C:\\Users")
+		So(err, ShouldEqual, nil)
+	})
+
+	Convey("Test dirWindows and error isn't nil.", t, func() {
+		p := ApplyFunc(os.Getenv, func(_ string) string {
+			return ""
+		})
+		defer p.Reset()
+
+		home, err := dirWindows()
+		So(home, ShouldEqual, "")
+		So(err, ShouldResemble, errors.New("Can't find 'USERPROFILE' environment variable"))
+	})
+}
+
+func TestDirUnix(t *testing.T) {
+	Convey("Test dirUnix and error is nil.", t, func() {
+		p := ApplyFunc(os.Getenv, func(_ string) string {
+			return "~"
+		})
+		defer p.Reset()
+
+		home, err := dirUnix()
+		So(home, ShouldEqual, "~")
+		So(err, ShouldEqual, nil)
+	})
+
+	Convey("Test dirUnix and error isn't nil.", t, func() {
+		p := ApplyFunc(os.Getenv, func(_ string) string {
+			return ""
+		})
+		defer p.Reset()
+
+		home, err := dirUnix()
+		So(home, ShouldEqual, "")
+		So(err, ShouldResemble, errors.New("Can't find 'HOME' environment variable"))
 	})
 }
