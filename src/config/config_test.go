bd14d80587f99ae7c629f776aaa494e534b8c14f
huiyifyj <jxfengyijie@gmail.com>
2020-09-03 01:09:19 +0800

    Increase test coverage for DBUrl function

--- src/config/config_test.go
+++ src/config/config_test.go
@@ -3,15 +3,55 @@ package config
 import (
 	"testing"
 
+	. "github.com/agiledragon/gomonkey"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestDBUrl(t *testing.T) {
-	Convey("Test DB url", t, func() {
-		want := "root:password@tcp(127.0.0.1:3306)/fyj?charset=utf8"
+	Convey("Test DBUrl when database is MySQL", t, func() {
+		ApplyFunc(getDBConfiguration, func() *DBConfiguration {
+			return &DBConfiguration{Type: "MySQL"}
+		})
+
+		want := ":@tcp(:)/?charset=utf8"
 		dbType, dsn := DBUrl()
 
 		So(dbType, ShouldEqual, "mysql")
 		So(dsn, ShouldEqual, want)
+	})
+
+	Convey("Test DBUrl when database is PostgreSQL", t, func() {
+		ApplyFunc(getDBConfiguration, func() *DBConfiguration {
+			return &DBConfiguration{Type: "PostgreSQL"}
+		})
+
+		want := "dbname= user= password= host= port="
+		dbType, dsn := DBUrl()
+
+		So(dbType, ShouldEqual, "postgres")
+		So(dsn, ShouldEqual, want)
+	})
+
+	Convey("Test DBUrl when database is SQLite", t, func() {
+		ApplyFunc(getDBConfiguration, func() *DBConfiguration {
+			return &DBConfiguration{Type: "SQLite3"}
+		})
+
+		want := "file:?cache=shared&mode=rwc"
+		dbType, dsn := DBUrl()
+
+		So(dbType, ShouldEqual, "sqlite3")
+		So(dsn, ShouldEqual, want)
+	})
+
+	Convey("Test DBUrl when database is not supported", t, func() {
+		ApplyFunc(getDBConfiguration, func() *DBConfiguration {
+			return &DBConfiguration{}
+		})
+
+		dbType, dsn := DBUrl()
+
+		So(dbType, ShouldEqual, "")
+		So(dsn, ShouldEqual, "")
 	})
 }
