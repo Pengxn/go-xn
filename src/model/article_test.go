13ed077beec671e0745c13a6e2d4c2bc26a8f8bf
huiyifyj <jxfengyijie@gmail.com>
2020-11-20 14:38:23 +0800

    Add more test cases
    - Add more test cases for model package.
    - Add 'deleted' tag for DeleteTime field.

---
+++ src/model/article_test.go
@@ -0,0 +1,63 @@
+package model
+
+import (
+	"os"
+
+	"github.com/go-ini/ini"
+	"xorm.io/xorm"
+
+	"github.com/Pengxn/go-xn/src/util/home"
+	"github.com/Pengxn/go-xn/src/util/log"
+)
+
+func init() {
+	CleanTable(testORM, &Article{})
+	db := testORM.NewSession()
+	defer db.Close()
+	if num, err := db.Insert(
+		&Article{ID: 1, Title: "One", Content: "1: Content One", Status: 1},
+		&Article{ID: 2, Title: "Two", Content: "2: Content Two", Status: 0},
+		&Article{ID: 3, Title: "Three", Content: "3: Content Three", Status: 1},
+		&Article{ID: 4, Title: "Four", Content: "4: Content Four", Status: 2},
+		&Article{ID: 5, Title: "Five", Content: "5: Content Five", Status: 1},
+		&Article{ID: 6, Title: "Six", Content: "6: Content Six", Status: 1},
+		&Article{ID: 7, Title: "Seven", Content: "7: Content Seven", Status: 0},
+		&Article{ID: 8, Title: "Eight", Content: "8: Content Eight", Status: 1},
+		&Article{ID: 9, Title: "Nine", Content: "9: Content Nine", Status: 1},
+	); err != nil {
+		log.Infof("Init data to article table error: %+v, num: %d", err, num)
+	}
+}
+
+var testORM = func() *xorm.Engine {
+	config, err := ini.Load(home.HomeDir() + string(os.PathSeparator) + "fyj.ini")
+	if err != nil {
+		log.Fatalln("Fail to read fyj.ini file.", err)
+	}
+
+	dbType := config.Section("test").Key("dbType").String()
+	uri := config.Section("test").Key("dbURI").String()
+	if uri == "" || dbType == "" {
+		dbType = "sqlite3"
+		uri = "./test.db?cache=shared&mode=rwc"
+	}
+
+	testDB, err := xorm.NewEngine(dbType, uri)
+	if err != nil {
+		log.Fatalln("Fail to connect test database.")
+	}
+
+	return testDB
+}()
+
+// CleanTable that is used to drop tables for test.
+func CleanTable(orm *xorm.Engine, modelTable interface{}) {
+	if has, _ := orm.IsTableExist(modelTable); has {
+		if err := orm.DropTables(modelTable); err != nil {
+			log.Fatalf("Drop table error: %+v, model: %+v", err, modelTable)
+		}
+	}
+	if err := orm.CreateTables(modelTable); err != nil {
+		log.Fatalf("Create table error: %+v, model: %+v", err, modelTable)
+	}
+}
---
+++ src/model/option_test.go
@@ -0,0 +1,127 @@
+package model
+
+import (
+	"testing"
+
+	. "github.com/agiledragon/gomonkey"
+	. "github.com/smartystreets/goconvey/convey"
+
+	"github.com/Pengxn/go-xn/src/util/log"
+)
+
+func init() {
+	CleanTable(testORM, &Option{})
+	db := testORM.NewSession()
+	defer db.Close()
+	num, err := db.Insert(
+		&Option{Name: "site_url", Value: "https://xn--02f.com", ID: 1},
+		&Option{Name: "will_be_deleted", Value: "data_will_be_deleted"},
+		&Option{Name: "will_be_update", Value: "data_will_be_update"},
+		&Option{Name: "test_exist", Value: "1"},
+	)
+	if err != nil {
+		log.Infof("Init data to option table error: %+v, num: %d", err, num)
+	}
+}
+
+func TestGetAllOptions(t *testing.T) {
+	p := ApplyGlobalVar(&orm, testORM)
+	defer p.Reset()
+
+	Convey("Test GetAllOptions.", t, func() {
+		So(GetAllOptions(), ShouldResemble, []Option{
+			{Name: "site_url", Value: "https://xn--02f.com"},
+			{Name: "will_be_deleted", Value: "data_will_be_deleted"},
+			{Name: "will_be_update", Value: "data_will_be_update"},
+			{Name: "test_exist", Value: "1"},
+		})
+	})
+}
+
+func TestGetOptionByName(t *testing.T) {
+	p := ApplyGlobalVar(&orm, testORM)
+	defer p.Reset()
+
+	Convey("When option record is exists.", t, func() {
+		has, option := GetOptionByName("site_url")
+
+		So(has, ShouldEqual, true)
+		So(option, ShouldResemble, Option{Name: "site_url", Value: "https://xn--02f.com"})
+	})
+
+	Convey("When option record does not exist.", t, func() {
+		has, option := GetOptionByName("not_exist")
+
+		So(has, ShouldEqual, false)
+		So(option, ShouldResemble, Option{})
+	})
+}
+
+func TestAddToOption(t *testing.T) {
+	p := ApplyGlobalVar(&orm, testORM)
+	defer p.Reset()
+
+	Convey("Add option record successfully.", t, func() {
+		success := AddToOption(&Option{Name: "add_record", Value: "add_data"})
+		So(success, ShouldEqual, true)
+
+		db := testORM.NewSession()
+		defer db.Close()
+
+		option := Option{Name: "add_record"}
+		_, err := db.Omit("option_id").Get(&option)
+		So(option, ShouldResemble, Option{Name: "add_record", Value: "add_data"})
+		So(err, ShouldBeNil)
+	})
+}
+
+func TestDeleteOptionByName(t *testing.T) {
+	p := ApplyGlobalVar(&orm, testORM)
+	defer p.Reset()
+
+	Convey("Delete option record successfully.", t, func() {
+		success := DeleteOptionByName("will_be_deleted")
+		So(success, ShouldEqual, true)
+
+		db := testORM.NewSession()
+		defer db.Close()
+
+		option := []Option{}
+		err := db.Where("option_name = ?", "will_be_deleted").Find(&option)
+		So(len(option), ShouldEqual, 0)
+		So(err, ShouldBeNil)
+	})
+}
+
+func TestUpdateOptionByName(t *testing.T) {
+	p := ApplyGlobalVar(&orm, testORM)
+	defer p.Reset()
+
+	Convey("Update option record successfully.", t, func() {
+		success := UpdateOptionByName(&Option{Name: "will_be_update", Value: "update_data"})
+		So(success, ShouldEqual, true)
+
+		db := testORM.NewSession()
+		defer db.Close()
+
+		option := Option{Name: "will_be_update"}
+		_, err := db.Omit("option_id").Get(&option)
+		So(option, ShouldResemble, Option{Name: "will_be_update", Value: "update_data"})
+		So(err, ShouldBeNil)
+	})
+}
+
+func TestOptionExist(t *testing.T) {
+	p := ApplyGlobalVar(&orm, testORM)
+	defer p.Reset()
+
+	Convey("When option record is exists.", t, func() {
+		success := OptionExist("test_exist")
+		So(success, ShouldEqual, true)
+	})
+
+	Convey("When option record does not exist.", t, func() {
+		success := OptionExist("not_exist")
+		So(success, ShouldEqual, false)
+	})
+}
