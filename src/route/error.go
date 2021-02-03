402ba90c760fc6d9ce1ee228c1e199bcb2bb3705
huiyifyj <jxfengyijie@gmail.com>
2021-02-03 22:59:50 +0800

    Update NoRoute handler for gin, return html

--- src/route/error.go
+++ src/route/error.go
@@ -10,17 +10,10 @@ func errorRoute(g *gin.Engine) {
 
 	// No route
 	g.NoRoute(func(c *gin.Context) {
-		if c.ContentType() == "text/html" {
-			c.HTML(404, "error.html", gin.H{
-				"code": 404,
-				"data": "The Page Could Not be Found",
-			})
-		} else {
-			c.JSON(404, gin.H{
-				"code": 404,
-				"data": "The Route Could Not be Found",
-			})
-		}
+		c.HTML(404, "error.html", gin.H{
+			"code": 404,
+			"data": "The Page Could Not be Found",
+		})
 	})
 
 	// No Method
