c9873461fb0486e030fec662474d80af1f7ea6e8
huiyifyj <jxfengyijie@gmail.com>
2021-03-24 00:43:16 +0800

    Upload robots.txt for web crawlers and robots

--- src/route/static.go
+++ src/route/static.go
@@ -7,6 +7,7 @@ import (
 // staticRoutes registers static routes files and folders.
 func staticRoutes(g *gin.Engine) {
 	g.StaticFile("/favicon.ico", "web/icons/xn-02f.png")
+	g.StaticFile("/robots.txt", "web/robots.txt")
 
 	g.Static("/css", "web/css")
 	g.Static("/js", "web/js")
