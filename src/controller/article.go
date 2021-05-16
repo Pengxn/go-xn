00abde32c38358a3624104fc3d2fa7e55e43ae78
huiyifyj <jxfengyijie@gmail.com>
2021-05-16 22:53:34 +0800

    Move the code to the right place

--- src/controller/article.go
+++ src/controller/article.go
@@ -8,6 +8,43 @@ import (
 	"github.com/Pengxn/go-xn/src/model"
 )
 
+// DefaultPageLimit is default limit number per page,
+// and other constant.
+const (
+	DefaultPageLimit int = 8
+)
+
+// HomePage returns home and index page.
+// Request sample:
+//     GET => /
+//     GET => /page/1
+func HomePage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"code": 200,
+		"data": model.ArticlesByPage(DefaultPageLimit, 1),
+	})
+}
+
+// ArticlesPage returns JSON information by page number.
+// Request sample:
+//     GET => /page/1?limit=6
+func ArticlesPage(c *gin.Context) {
+	pageNum, _ := strconv.Atoi(c.Param("pageNum"))
+	limitNum, _ := strconv.Atoi(c.Query("limit"))
+
+	if pageNum <= 0 {
+		pageNum = 1
+	}
+	if limitNum == 0 {
+		limitNum = DefaultPageLimit
+	}
+
+	c.JSON(200, gin.H{
+		"code": 200,
+		"data": model.ArticlesByPage(limitNum, pageNum),
+	})
+}
+
 // ListArticles returns the number of all articles.
 // Request sample:
 //     GET => /articles
