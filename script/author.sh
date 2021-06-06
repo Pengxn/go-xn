ee04bb6d26a1b91189137c72fd4d9850e11df9dd
huiyifyj <jxfengyijie@gmail.com>
2021-06-06 13:50:49 +0800

    Make sure script runs in the root directory

--- script/author.sh
+++ script/author.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+CURRENT_DIR="$(cd "$(dirname $0)"; pwd)"
+ROOT_DIR=$CURRENT_DIR/..
+
+cd $ROOT_DIR
+
 # Generate AUTHORS file in build directory
 echo "Here is an inevitably incomplete list of MUCH-APPRECIATED CONTRIBUTORS --
 people who have submitted patches, reported bugs, added translations, helped
