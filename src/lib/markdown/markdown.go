7e10eeb4d6e5d6a837c3f1b999dfb0f36a562206
huiyifyj <jxfengyijie@gmail.com>
2023-04-13 23:45:12 +0800

    Add markdown parse test

--- src/lib/markdown/markdown.go
+++ src/lib/markdown/markdown.go
@@ -10,6 +10,7 @@ import (
 // ToHTML converts markdown content to HTML.
 func ToHTML(content []byte) ([]byte, error) {
 	md := goldmark.New(goldmark.WithExtensions(extension.GFM))
+
 	var buf bytes.Buffer
 	if err := md.Convert(content, &buf); err != nil {
 		return nil, err
---
+++ src/lib/markdown/markdown_test.go
@@ -0,0 +1,25 @@
+package markdown
+
+import (
+	"testing"
+
+	// . "github.com/agiledragon/gomonkey/v2"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestToHTML(t *testing.T) {
+	md := `# Hello, World!
+> This is test.
+`
+	html := `<h1>Hello, World!</h1>
+<blockquote>
+<p>This is test.</p>
+</blockquote>
+`
+
+	Convey("Test ToHTML.", t, func() {
+		h, err := ToHTML([]byte(md))
+		So(h, ShouldResemble, []byte(html))
+		So(err, ShouldBeNil)
+	})
+}
