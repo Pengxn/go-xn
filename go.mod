56190dfd36248596d5afc4d6077e51708a47560f
huiyifyj <jxfengyijie@gmail.com>
2023-04-20 19:57:49 +0800

    Support to interact with redis cache
    - Add [go-redis](github.com/redis/go-redis/v9) dependency library to interact with redis.
    - Implement `Add/Set/Get/Delete` function as same as commit 5d386be .

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.16
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/pquerna/otp v1.4.0
+	github.com/redis/go-redis/v9 v9.0.3
 	github.com/rs/cors v1.9.0
 	github.com/schollz/progressbar/v3 v3.13.1
 	github.com/sirupsen/logrus v1.9.0
@@ -28,8 +29,10 @@ go 1.18
 require (
 	github.com/boombuler/barcode v1.0.1 // indirect
 	github.com/bytedance/sonic v1.8.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
---
+++ src/lib/cache/redis/redis.go
@@ -0,0 +1,66 @@
+package redis
+
+import (
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+
+	"github.com/Pengxn/go-xn/src/config"
+)
+
+var (
+	// UseRedis indicates whether to use redis.
+	UseRedis bool
+
+	r *redis.Client
+)
+
+func init() {
+	if config.Config.Redis.URL != "" {
+		UseRedis = true
+	}
+
+	r = redis.NewClient(&redis.Options{
+		Addr:     config.Config.Redis.URL,
+		Password: config.Config.Redis.Password,
+		DB:       config.Config.Redis.DB,
+	})
+}
+
+// Add adds a key/value pair to the redis only if the key doesn't already exist,
+// or if the key has expired. Returns an error otherwise.
+func Add(key string, value interface{}) error {
+	return r.Set(context.Background(), key, value, 5*time.Minute).Err()
+}
+
+// Set sets a key/value pair in the reids. If the key already exists, it will be
+// overwritten.
+func Set(key string, value interface{}) {
+	r.Set(context.Background(), key, value, 5*time.Minute)
+}
+
+// SetWithExpiration sets a key/value pair in the reids with a given expiration.
+// If the key already exists, it will be overwritten. If expiration is 0, the
+// default expiration time will be used.
+func SetWithExpiration(key string, value interface{}, expiration time.Duration) {
+	r.Set(context.Background(), key, value, expiration)
+}
+
+// Get gets a value by key from the redis. It returns the value and a boolean
+// indicating whether the key was found in the redis.
+func Get(key string) (interface{}, bool) {
+	result, err := r.Get(context.Background(), key).Result()
+	if err == redis.Nil {
+		return "", false
+	} else if err != nil {
+		return "", false
+	}
+
+	return result, true
+}
+
+// Delete deletes a key/value pair by key from the redis whether it exists or not.
+func Delete(key string) {
+	r.Del(context.Background(), key)
+}
