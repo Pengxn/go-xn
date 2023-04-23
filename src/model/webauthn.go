d263553ddd2a854c5f759a7d644f4f910d40eab1
huiyifyj <jxfengyijie@gmail.com>
2023-04-23 19:02:08 +0800

    Remove `WebAuthnSession` model struct and its methods
    - Remove the `WebAuthnSession` model struct and its methods.
    - Update table column name for `xorm` struct tag.
    
    Refer to links below:
    https://gitea.com/xorm/website2/src/branch/main/content/en/docs/chapter-02/1.mapping.md
     https://xorm.io/docs/chapter-02/4.columns/

--- src/model/webauthn.go
+++ src/model/webauthn.go
@@ -2,43 +2,21 @@ package model
 
 import "time"
 
-// WebAuthnSession represents the WebAuthn session data.
-type WebAuthnSession struct {
-	ID          int64      `json:"ID" xorm:"pk autoincr 'ID'"`
-	UserID      int64      `json:"user_id" xorm:"INDEX 'user_id'"`
-	Challenge   []byte     `json:"challenge" xorm:"INDEX VARBINARY(1024) 'challenge'"`
-	CreatedTime *time.Time `json:"created_unix" xorm:"created 'created_time'"`
-	UpdatedTime *time.Time `json:"updated_unix" xorm:"updated 'updated_time'"`
-}
-
-// TableName returns a better table name for WebAuthnSession.
-func (ws WebAuthnSession) TableName() string {
-	return "webauthn_session"
-}
-
-// Add adds a temporary WebAuthn session data.
-func (ws WebAuthnSession) Add() {
-	// TODO
-}
-
-func (ws WebAuthnSession) GetByChallenge(challenge []byte) {
-	// TODO
-}
-
 // WebAuthnCredential represents the WebAuthn credential data.
 type WebAuthnCredential struct {
-	ID              int64      `json:"ID" xorm:"pk autoincr"`
-	Name            string     `json:"name" xorm:"unique(s)"`
+	ID              int64      `json:"ID" xorm:"pk autoincr 'ID'"`
+	Name            string     `json:"name" xorm:"unique(s) 'name'"`
 	NickName        string     `json:"nick_name" xorm:"varchar(25) notnull 'nick_name'"`
-	UserID          int64      `json:"user_id" xorm:"INDEX unique(s)"`
-	CredentialID    []byte     `json:"credential_id" xorm:"INDEX VARBINARY(1024)"`
-	PublicKey       []byte     `json:"public_key"`
-	AttestationType string     `json:"attestation_type"`
-	AAGUID          []byte     `json:"aaguid"`
-	SignCount       uint32     `json:"sign_count" xorm:"BIGINT"`
-	CloneWarning    bool       `json:"clone_warning"`
-	CreatedTime     *time.Time `json:"created_unix" xorm:"INDEX created"`
-	UpdatedTime     *time.Time `json:"updated_unix" xorm:"updated"`
+	UserID          int64      `json:"user_id" xorm:"INDEX unique(s) notnull 'user_id'"`
+	CredentialID    []byte     `json:"credential_id" xorm:"INDEX VARBINARY(1024) notnull 'credential_id'"`
+	PublicKey       []byte     `json:"public_key" xorm:"notnull 'public_key'"`
+	AttestationType string     `json:"attestation_type" xorm:"'attestation_type'"`
+	AAGUID          []byte     `json:"aaguid" xorm:"notnull 'aaguid'"`
+	SignCount       uint32     `json:"sign_count" xorm:"BIGINT 'sign_count'"`
+	CloneWarning    bool       `json:"clone_warning" xorm:"'clone_warning'"`
+	Attachment      string     `json:"attachment" xorm:"'attachment'"`
+	CreatedTime     *time.Time `json:"created_time,omitempty" xorm:"INDEX created 'created_time'"`
+	UpdatedTime     *time.Time `json:"updated_time,omitempty" xorm:"updated 'updated_time'"`
 }
 
 // TableName returns a better table name for WebAuthnCredential.
