d3f37069b0c1e99edb168771d81021a5d5b5b27b
huiyifyj <jxfengyijie@gmail.com>
2023-10-22 20:21:00 +0800

    Update tests with more detailed data
    Update the `getDBUrl` function tests with more detailed data.

--- src/util/db/db_test.go
+++ src/util/db/db_test.go
@@ -12,10 +12,17 @@ import (
 func TestDBUrl(t *testing.T) {
 	Convey("Test if DB Url is correct.", t, func() {
 		Convey("Test DBUrl when database is MySQL", func() {
-			patch := ApplyGlobalVar(&config.Config.DB, config.DBConfig{Type: "MySQL"})
+			patch := ApplyGlobalVar(&config.Config.DB, config.DBConfig{
+				Type:     "MySQL",
+				User:     "root",
+				Password: "",
+				Port:     "3306",
+				Name:     "test",
+				Url:      "localhost",
+			})
 			defer patch.Reset()
 
-			want := ":@tcp(:)/?charset=utf8"
+			want := "root:@tcp(localhost:3306)/test?charset=utf8"
 			dbType, dsn := getDBUrl()
 
 			So(dbType, ShouldEqual, "mysql")
@@ -23,10 +30,18 @@ func TestDBUrl(t *testing.T) {
 		})
 
 		Convey("Test DBUrl when database is PostgreSQL", func() {
-			patch := ApplyGlobalVar(&config.Config.DB, config.DBConfig{Type: "PostgreSQL"})
+			patch := ApplyGlobalVar(&config.Config.DB, config.DBConfig{
+				Type:     "PostgreSQL",
+				User:     "postgres",
+				Password: "",
+				Port:     "5432",
+				Name:     "test",
+				Url:      "localhost",
+				SSLMode:  "disable",
+			})
 			defer patch.Reset()
 
-			want := "dbname= user= password= host= port= sslmode=disable"
+			want := "dbname=test user=postgres password= host=localhost port=5432 sslmode=disable"
 			dbType, dsn := getDBUrl()
 
 			So(dbType, ShouldEqual, "postgres")
@@ -34,10 +49,13 @@ func TestDBUrl(t *testing.T) {
 		})
 
 		Convey("Test DBUrl when database is SQLite", func() {
-			patch := ApplyGlobalVar(&config.Config.DB, config.DBConfig{Type: "SQLite3"})
+			patch := ApplyGlobalVar(&config.Config.DB, config.DBConfig{
+				Type: "SQLite3",
+				Name: "test.db",
+			})
 			defer patch.Reset()
 
-			want := "file:?cache=shared&mode=rwc"
+			want := "file:test.db?cache=shared&mode=rwc"
 			dbType, dsn := getDBUrl()
 
 			So(dbType, ShouldEqual, "sqlite3")
