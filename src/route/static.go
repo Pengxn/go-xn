563de4ecada60f36d7950035a15a2abbd54c6a67
Feng.YJ <32027253+huiyifyj@users.noreply.github.com>
2025-01-11 18:56:28 +0800

    Refactor static files handling and web embed FS constants (#341)
    * Move HTML template handling to route package
    
    - Move `HTML()` template functionality from `web/embed.go` to `route/static.go`.
    - Rename embedFS to EmbedFS and make it public.
    - Update related references for template parsing.
    
    * Move assets filesystem handler to route package
    
    - Move `FS()` from web package to route package and rename to `assetsFS()`.
    - Improve code organization by keeping filesystem handlers close to routes.
    
    * Simplify embedded filesystem handling
    
    - Remove `OtherFS()` function and directly expose the `Other` embed.FS.
    - Move embedded filesystem variable declarations into a single var block.
    - Update static routes to use http.FS(web.Other) directly by type conversion.
    
    This change simplifies the code by removing an unnecessary wrapper function and making the embedded filesystem more directly accessible.

--- src/route/static.go
+++ src/route/static.go
@@ -1,20 +1,24 @@
 package route
 
 import (
+	"fmt"
+	"html/template"
+	"io/fs"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 
+	"github.com/Pengxn/go-xn/src/util/log"
 	"github.com/Pengxn/go-xn/web"
 )
 
 // staticRoutes registers static routes files and folders.
 func staticRoutes(g *gin.Engine) {
-	g.SetHTMLTemplate(web.HTML(gin.IsDebugging()))
+	g.SetHTMLTemplate(templateFromFS(web.EmbedFS))
 
-	g.StaticFS("/assets", web.FS())
+	g.StaticFS("/assets", assetsFS())
 
-	otherFS := web.OtherFS()
+	otherFS := http.FS(web.Other)
 	staticFileFromFS(g, "/favicon.ico", "icons/logo.ico", otherFS)
 	// Robots.txt, refer to https://www.robotstxt.org/
 	staticFileFromFS(g, "/robots.txt", "robots.txt", otherFS)
@@ -28,4 +32,35 @@ func staticFileFromFS(g *gin.Engine, relativePath, filepath string, fs http.File
 	}
 	g.GET(relativePath, handler)
 	g.HEAD(relativePath, handler)
+}
+
+// HTML returns the HTML template.
+func templateFromFS(fsys fs.FS) *template.Template {
+	templates, err := fs.Sub(fsys, "templates")
+	if err != nil {
+		log.Errorf("HTML fs.Sub error: %+v", err)
+	}
+
+	t, err := template.ParseFS(templates, "*.html", "**/*.html")
+	if err != nil {
+		log.Errorf("HTML template.ParseFS error: %+v", err)
+	}
+
+	if gin.IsDebugging() {
+		for _, tmpl := range t.Templates() {
+			fmt.Println(tmpl.Name())
+		}
+	}
+
+	return t
+}
+
+// assetsFS returns the assets file system.
+func assetsFS() http.FileSystem {
+	assets, err := fs.Sub(web.EmbedFS, "assets")
+	if err != nil {
+		log.Errorf("FS fs.Sub error: %+v", err)
+	}
+
+	return http.FS(assets)
 }
--- web/embed.go
+++ web/embed.go
@@ -2,60 +2,18 @@ package web
 
 import (
 	"embed"
-	"fmt"
-	"html/template"
-	"io/fs"
-	"net/http"
-
-	"github.com/Pengxn/go-xn/src/util/log"
 )
 
-//go:embed templates
-//go:embed assets
-var embedFS embed.FS
-
-// HTML returns the HTML template.
-func HTML(debug bool) *template.Template {
-	templates, err := fs.Sub(embedFS, "templates")
-	if err != nil {
-		log.Errorf("HTML fs.Sub error: %+v", err)
-	}
-
-	t, err := template.ParseFS(templates, "*.html", "**/*.html")
-	if err != nil {
-		log.Errorf("HTML template.ParseFS error: %+v", err)
-	}
-
-	if debug {
-		for _, tmpl := range t.Templates() {
-			fmt.Println(tmpl.Name())
-		}
-	}
-
-	return t
-}
-
-// FS returns the `assets` file system.
-func FS() http.FileSystem {
-	assets, err := fs.Sub(embedFS, "assets")
-	if err != nil {
-		log.Errorf("FS fs.Sub error: %+v", err)
-	}
-
-	return http.FS(assets)
-}
-
-//go:embed robots.txt
-//go:embed humans.txt
-//go:embed icons
-var other embed.FS
-
-// OtherFS returns the `other` file system.
-func OtherFS() http.FileSystem {
-	return http.FS(other)
-}
-
 var (
+	//go:embed templates
+	//go:embed assets
+	EmbedFS embed.FS
+
+	//go:embed robots.txt
+	//go:embed humans.txt
+	//go:embed icons
+	Other embed.FS
+
 	//go:embed templates/mail/mail-login.html
 	MailLogin string
 	//go:embed templates/mail/mail-change.html
