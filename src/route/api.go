272e0ffefa5248cb9e68c2548458f86d7dbe2947
Feng.YJ <32027253+huiyifyj@users.noreply.github.com>
2025-01-21 20:59:52 +0800

    Improve API routes with `JWT` middleware (#351)
    * Improve API routes with `JWT` middleware
    
    - Add JWT claims and token generation utility functions.
    - Create and return JWT token for endpoint at `/api/token`.
    - Update admin API routes with `JWT` authentication middleware.
    - Enhance JWT middleware to add `uid` and to context.
    - Implement JWT token generation in `src/lib/jwt` package.
    
    * Clarify JWT expiration time unit in config
    
    - Fix import ordering in `jwt` package, thrid-party packages should be imported before local packages, and separated by a blank line.
    - Update JWT expiration time comment to specify unit as days, improve config documentation readability.

--- src/route/api.go
+++ src/route/api.go
@@ -1,13 +1,18 @@
 package route
 
 import (
-	"github.com/Pengxn/go-xn/src/api"
 	"github.com/gin-gonic/gin"
+
+	"github.com/Pengxn/go-xn/src/api"
+	"github.com/Pengxn/go-xn/src/middleware"
 )
 
 func apiRoutes(g *gin.Engine) {
 	adminAPI := api.NewAdminAPI()
 
-	api := g.Group("/api")
+	// API without JWT authentication
+	g.GET("/api/token", adminAPI.Token)
+
+	api := g.Group("/api").Use(middleware.JWT())
 	api.POST("/admin", adminAPI.RegisterAdmin)
 }
