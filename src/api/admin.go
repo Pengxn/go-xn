7b56bb299c05de3a33c32ef48c3c440e0185b94a
huiyifyj <jxfengyijie@gmail.com>
2025-01-21 14:43:46 +0800

    Improve API routes with `JWT` middleware
    - Add JWT claims and token generation utility functions.
    - Create and return JWT token for endpoint at `/api/token`.
    - Update admin API routes with `JWT` authentication middleware.
    - Enhance JWT middleware to add `uid` and to context.
    - Implement JWT token generation in `src/lib/jwt` package.

--- src/api/admin.go
+++ src/api/admin.go
@@ -1,11 +1,34 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/Pengxn/go-xn/src/lib/jwt"
+)
 
 type adminAPI struct{}
 
 func NewAdminAPI() *adminAPI {
 	return &adminAPI{}
+}
+
+func (a *adminAPI) Token(c *gin.Context) {
+	claims := jwt.NewClaims(1, "admin")
+	token, err := jwt.TokenFromClaims(claims)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code":    http.StatusInternalServerError,
+			"message": "token generation failed",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code":  http.StatusOK,
+		"token": token,
+	})
 }
 
 func (a *adminAPI) RegisterAdmin(c *gin.Context) {
---
+++ src/lib/jwt/jwt.go
@@ -0,0 +1,37 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/Pengxn/go-xn/src/config"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Claims struct {
+	UID      int    `json:"uid"`
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func NewClaims(uid int, username string) Claims {
+	now := time.Now()
+
+	claims := Claims{
+		UID:      uid,
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			IssuedAt: jwt.NewNumericDate(now),
+		},
+	}
+	// Set expiration time if config value is set, the value is in days.
+	if config.Config.Server.JwtExp > 0 {
+		claims.ExpiresAt = jwt.NewNumericDate(now.AddDate(0, 0, config.Config.Server.JwtExp))
+	}
+
+	return claims
+}
+
+func TokenFromClaims(claims Claims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(config.Config.Server.JwtToken))
+}
