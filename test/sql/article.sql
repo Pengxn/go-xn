c19e0ea11c0b596f278c1a411c26879adbb49df6
huiyifyj <jxfengyijie@gmail.com>
2025-02-03 14:31:52 +0800

    Clean up database schema and deprecated test data
    - Update test user email address in user table.
    - Remove deprecated 'url' field from article table as 'slug' serves the same purpose.

--- test/sql/article.sql
+++ test/sql/article.sql
@@ -10,7 +10,6 @@
 DROP TABLE IF EXISTS `article`;
 CREATE TABLE `article` (
     `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    `url` varchar(255) NOT NULL COMMENT 'The url of article', -- deprecated
     `slug` varchar(255) NOT NULL COMMENT 'The url of article',
     `title` text NOT NULL COMMENT 'The title of article',
     `content` longtext NOT NULL COMMENT 'The content of article',
@@ -30,7 +29,6 @@ CREATE TABLE `article` (
 INSERT INTO `article` VALUES (
     '1',
     "we-have-to-put-a-barrier-between-us-and-the-world",
-    "we-have-to-put-a-barrier-between-us-and-the-world",
     'Everybody listen! We have to put a barrier between us and the world!',
     'At Aspire Themes I use a lot of tools to help me create WordPress, Ghost and Jekyll themes. Tools will range from development, design, servi',
     '0',
@@ -41,7 +39,6 @@ INSERT INTO `article` VALUES (
 );
 INSERT INTO `article` VALUES (
     '2',
-    'if-you-have-an-opportunity-to-use-your-voice-you-should-use-it',
     'if-you-have-an-opportunity-to-use-your-voice-you-should-use-it',
     'If you have an opportunity to use your voice you should use it',
     'This service is just awesome, I use the Ghost Stack to install Ghost locally. This saves a lot of time and headache installing Ghost, and by',
@@ -54,7 +51,6 @@ INSERT INTO `article` VALUES (
 INSERT INTO `article` VALUES (
     '3',
     'writing-posts-with-Ghost',
-    'writing-posts-with-Ghost',
     'Writing posts with Ghost ✍️',
     'Getting started with the editor is simple, with familiar formatting options in a functional toolbar and the ability to add dynamic content seamlessly.',
     '0',
@@ -65,7 +61,6 @@ INSERT INTO `article` VALUES (
 );
 INSERT INTO `article` VALUES (
     '4',
-    'gathered-here-on-our-god-given-much-naeeded-day',
     'gathered-here-on-our-god-given-much-naeeded-day',
     'The reason we are gathered here on our God-given, much-naeeded day.',
     'iTerm is a replacement to Mac terminal, and I think most of you are using it for Mac. Tmux is a terminal multilpexer and a pretty great tool',
@@ -78,7 +73,6 @@ INSERT INTO `article` VALUES (
 INSERT INTO `article` VALUES (
     '5',
     'two-antarctic-penguins-took-an-adorable-selfie',
-    'two-antarctic-penguins-took-an-adorable-selfie',
     'Two antarctic penguins took an adorable selfie',
     'Welcome, it is great to have you here. We know that first impressions are important, so we have populated your new site with some initial getting started posts that will help you get familiar with everything in no time.',
     '0',
@@ -89,7 +83,6 @@ INSERT INTO `article` VALUES (
 );
 INSERT INTO `article` VALUES (
     '6',
-    'slow-cooker-honey-dijon-glazed-carrots',
     'slow-cooker-honey-dijon-glazed-carrots',
     'Slow cooker honey-dijon glazed carrots',
     'Getting started with the editor is simple, with familiar formatting options in a functional toolbar and the ability to add dynamic content seamlessly.',
@@ -102,7 +95,6 @@ INSERT INTO `article` VALUES (
 INSERT INTO `article` VALUES (
     '7',
     'managing-admin-settings',
-    'managing-admin-settings',
     'Managing admin settings',
     'There are a couple of things to do next while you are getting set up: making your site private and inviting your team.',
     '0',
@@ -113,7 +105,6 @@ INSERT INTO `article` VALUES (
 );
 INSERT INTO `article` VALUES (
     '8',
-    'nibh-labore-ac-condimentum-sequi-ullam',
     'nibh-labore-ac-condimentum-sequi-ullam',
     'Nibh labore ac condimentum sequi ullam',
     'Porro! Mollitia earum congue aliquid? Doloribus. Sociosqu hymenaeos! Ultrices, placerat accusantium iaculis? Irure voluptatibus accumsan odio? Aut, id hymenaeos officia reiciendis dictumst necessitatibus netus, voluptates doloribus porro sodales, eleifend! Mollis.',
@@ -126,7 +117,6 @@ INSERT INTO `article` VALUES (
 INSERT INTO `article` VALUES (
     '9',
     'dicta-montes-ac-doloremque-xercitation',
-    'dicta-montes-ac-doloremque-xercitation',
     'Dicta montes ac doloremque? Exercitation',
     'Welcome, it is great to have you here.We know that first impressions are important, so we have populated your new site with some initial getting started posts that will help you',
     '0',
@@ -138,7 +128,6 @@ INSERT INTO `article` VALUES (
 INSERT INTO `article` VALUES (
     '10',
     'vivamus-aliqua-ridiculus',
-    'vivamus-aliqua-ridiculus',
     'Molestie nostra consequatur. Vivamus aliqua ridiculus',
     'The Ghost editor has everything you need to fully optimise your content. This is where you can add tags and authors, feature a post, or turn a post into a',
     '0',
@@ -149,7 +138,6 @@ INSERT INTO `article` VALUES (
 );
 INSERT INTO `article` VALUES (
     '11',
-    'chinese-words-test',
     'chinese-words-test',
     '中文文本测试',
     '测试中文显示效果，仅用于展示文本显示及字体等效果。',
