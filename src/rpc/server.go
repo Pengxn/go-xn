033b37010c75c1289f6f1f2aa999c7325c3cb870
Feng.YJ <32027253+huiyifyj@users.noreply.github.com>
2025-02-14 08:56:52 +0800

    Implement agent command to run gRPC server (#377)
    - Rename health check method from `HeartBeat` to `Ping` for server side.
    - Add TCP listener setup and server initialization for gRPC server.
    - Use `agent` command to run gRPC server, and implement health check service registration.

--- src/rpc/server.go
+++ src/rpc/server.go
@@ -10,6 +10,6 @@ type Server struct {
 	pb.HeathCheckServer
 }
 
-func (s *Server) HeartBeat(_ context.Context, _ *pb.Empty) (*pb.Pong, error) {
+func (s *Server) Ping(_ context.Context, _ *pb.Empty) (*pb.Pong, error) {
 	return &pb.Pong{Message: "ok!"}, nil
 }
