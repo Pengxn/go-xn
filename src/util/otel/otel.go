b1147f33db28a045760186497f62d425d3894498
huiyifyj <jxfengyijie@gmail.com>
2025-04-19 16:30:56 +0800

    Add doc comments for `otel` package
    Add detailed doc comments for the `InitTrace()` function and `Config` struct for `github.com/Pengxn/go-xn/src/util/otel` package.

--- src/util/otel/otel.go
+++ src/util/otel/otel.go
@@ -15,6 +15,8 @@ import (
 	"go.opentelemetry.io/otel/sdk/trace"
 )
 
+// InitTrace initializes the OpenTelemetry trace exporter with the given config.
+// It returns a function to shut down the exporter when done.
 func InitTrace(ctx context.Context, c Config) func(context.Context) error {
 	var client otlptrace.Client
 	switch c.ClientType {
@@ -31,12 +33,22 @@ func InitTrace(ctx context.Context, c Config) func(context.Context) error {
 	return initOTELTracer(ctx, client)
 }
 
+// Config is the configuration for [OpenTelemetry].
+// It contains the client type, endpoint, and headers for the exporter.
+// The client type can be either "grpc" or "http".
+// The endpoint is the URL of the [OpenTelemetry Collector], default not including `v1/trace`.
+// refer to https://opentelemetry.io/docs/languages/sdk-configuration/otlp-exporter/
+//
+// [OpenTelemetry]: https://opentelemetry.io/
+// [OpenTelemetry Collector]: https://opentelemetry.io/docs/collector/
 type Config struct {
 	ClientType string
 	Endpoint   string
 	Headers    map[string]string
 }
 
+// newGRPCClient creates a new gRPC client for OpenTelemetry.
+// https://opentelemetry.io/docs/languages/go/exporters/#otlp-traces-over-http
 func newGRPCClient(endpoint string, headers map[string]string) otlptrace.Client {
 	return otlptracegrpc.NewClient(
 		otlptracegrpc.WithEndpoint(endpoint),
@@ -44,6 +56,8 @@ func newGRPCClient(endpoint string, headers map[string]string) otlptrace.Client 
 	)
 }
 
+// newHTTPClient creates a new HTTP client for OpenTelemetry.
+// https://opentelemetry.io/docs/languages/go/exporters/#otlp-traces-over-http
 func newHTTPClient(endpoint string, headers map[string]string) otlptrace.Client {
 	return otlptracehttp.NewClient(
 		otlptracehttp.WithEndpoint(endpoint),
@@ -51,12 +65,15 @@ func newHTTPClient(endpoint string, headers map[string]string) otlptrace.Client 
 	)
 }
 
+// initOTELTracer initializes the OpenTelemetry tracer with the given client.
 func initOTELTracer(ctx context.Context, client otlptrace.Client) func(context.Context) error {
+	// create the exporter
 	exporter, err := otlptrace.New(ctx, client)
 	if err != nil {
 		log.Fatalf("failed to create exporter: %s", err)
 	}
 
+	// create the resource
 	resources, err := resource.New(
 		context.Background(),
 		resource.WithAttributes(
@@ -69,6 +86,7 @@ func initOTELTracer(ctx context.Context, client otlptrace.Client) func(context.C
 		log.Fatalf("failed to set resources: %s", err)
 	}
 
+	// set the global OpenTelemetry tracer provider
 	otel.SetTracerProvider(
 		trace.NewTracerProvider(
 			trace.WithSampler(trace.AlwaysSample()),
