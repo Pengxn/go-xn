ebdcb809312df0c6d6e3b2e7f995f7e56a14582b
huiyifyj <jxfengyijie@gmail.com>
2025-04-27 13:38:01 +0800

    fix: check file if it exists before initializing writer
    Add a check using [os.Stat] to verify that the target log file exists before initializing the lumberjack logger.
    If the file does not exist, log an error and return [io.Discard] to avoid issues with invalid file path.
    
    Add TODO comments for customizing lumberjack settings and checking file mode and permissions.
    
    [os.Stat]: https://pkg.go.dev/os#Stat
    [io.Discard]: https://pkg.go.dev/io#Discard

--- src/util/slog/files_write.go
+++ src/util/slog/files_write.go
@@ -2,6 +2,8 @@ package slogger
 
 import (
 	"io"
+	"log/slog"
+	"os"
 
 	"gopkg.in/natefinch/lumberjack.v2"
 )
@@ -9,6 +11,14 @@ import (
 // NewFilesWriter creates a new rolling files writer.
 // It rotates the log file when it reaches a certain size, age...
 func NewFilesWriter(filename string) io.Writer {
+	// TODO: check the file mode and permissions
+	_, err := os.Stat(filename)
+	if err != nil || os.IsNotExist(err) {
+		slog.Error("file not exist", slog.String("filename", filename), slog.Any("error", err))
+		return io.Discard // or instead of it use os.Stdout
+	}
+
+	// TODO: customize lumberjack settings
 	return &lumberjack.Logger{
 		Filename:   filename,
 		MaxSize:    10, // megabytes
