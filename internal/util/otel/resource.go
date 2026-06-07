d5dd40e196bdb75e8c2ef79bb9e75ca262657a63
Feng.YJ <jxfengyijie@gmail.com>
2026-06-07 16:56:20 +0800

    Add `WithFromEnv` resource detector to otel resource initialization
    Add `resource.WithFromEnv()` to read OTEL_RESOURCE_ATTRIBUTES and other OTEL_* environment variables for resource auto-detection.
    refer to https://opentelemetry.io/docs/specs/otel/configuration/sdk-environment-variables/

--- internal/util/otel/resource.go
+++ internal/util/otel/resource.go
@@ -11,11 +11,12 @@ import (
 // commonResource returns a common resource with service name, OS, and arch.
 func commonResource(ctx context.Context) (*resource.Resource, error) {
 	return resource.New(ctx,
+		resource.WithFromEnv(),
+		resource.WithHost(),
 		resource.WithAttributes(
 			attribute.String("service.name", "go-xn"),
 			attribute.String("os.type", runtime.GOOS),
 			attribute.String("os.arch", runtime.GOARCH),
 		),
-		resource.WithHost(),
 	)
 }
